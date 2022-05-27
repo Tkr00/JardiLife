1500824855caa630271757651e4d061b6a405f5a
ByronRomanA <by.roman@duocuc.cl>
2022-05-27 12:07:10 -0400

    admin camibos

--- TestDjango/core/admin.py
+++ TestDjango/core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Categoria,Vehiculo
+from .models import Categoria,Arbustos,Flores,Maceteros,Tierra_De_Hojas
 # Register your models here.
 #Permite administrar el modelo completo
 
 admin.site.register(Categoria)
-admin.site.register(Vehiculo)
+admin.site.register(Arbustos)
+admin.site.register(Flores)
+admin.site.register(Maceteros)
+admin.site.register(Tierra_De_Hojas)
