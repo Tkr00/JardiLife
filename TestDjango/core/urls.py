e7e9f492235c10bf8ffa8090db82ee72bbeb2e13
ByronRomanA <by.roman@duocuc.cl>
2022-05-27 12:19:21 -0400

    url actualizada

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views  import home
+from .views  import Pagina, inicio, Flores, Macetero, Arbustos, Producto, quienes_somos, tierra_de_hojas
 
 urlpatterns = [
-    path('',home,name="home"),
+    path('pagina', Pagina, name= "Pagina" ),
+    path('inicio', inicio, name="inicio"),
+    path('flores', Flores, name="Flores"),
+    path('macetero', Macetero, name="Macetero"),
+    path('arbustos', Arbustos, name="Arbustos"),
+    path('producto', Producto, name="Producto"),
+    path('quienes-somos', quienes_somos, name= "quienes_somos"),
+    path('tierra-de-hojas', tierra_de_hojas,name= "tierra_de_hojas")
 ]
