f27cbceb79faf5f32975b304a0158cf16eb30dd5
ByronRomanA <by.roman@duocuc.cl>
2022-05-27 11:22:37 -0400

    ARCHIVOS DJANGO

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views  import home,form_vehiculo,form_mod_vehiculo,form_del_vehiculo
+from .views  import home
 
 urlpatterns = [
     path('',home,name="home"),
-    path('form-vehiculo',form_vehiculo,name='form_vehiculo'),
-    path('form-mod-vehiculo/<id>', form_mod_vehiculo,name='form_mod_vehiculo'),
-    path('form-del-vehiculo/<id>', form_del_vehiculo,name='form_del_vehiculo'),
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,55 +1,16 @@
-from django.shortcuts import render,redirect
-from .forms import VehiculoForm
-from .models import Vehiculo
+from django.shortcuts import render
 
 # Create your views here.
-
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+        super().__init__()
 
 
 def home(request):
-    vehiculos = Vehiculo.objects.all()
-
-    datos = {
-        'vehiculos': vehiculos
-    }
-    return render(request,'core/home.html',datos)
-
-def form_vehiculo(request):
     
-    datos = {
-        'form':VehiculoForm()
-    }
-
-    if request.method=='POST':
-        formulario =  VehiculoForm(request.POST)
-
-        if formulario.is_valid:
-            formulario.save()
-            datos['mensaje'] = "Guardado Correctamente";
-
-    return render(request,'core/form_vehiculo.html',datos)
-
-def form_mod_vehiculo(request,id):
-
-    vehiculo = Vehiculo.objects.get(patente=id)
-
-    datos = {
-        'form': VehiculoForm(instance=vehiculo)
-    }
-
-    if request.method == 'POST':
-        formulario = VehiculoForm(data=request.POST,instance=vehiculo)
-        if formulario.is_valid:
-            formulario.save()
-            datos['mensaje'] = "Modificacion realizada Correctamente"
-
-
-    return render(request, 'core/form_mod_vehiculo.html',datos)
-
-def form_del_vehiculo(request,id):
-
-    vehiculo = Vehiculo.objects.get(patente=id)
-
-    vehiculo.delete()
-
-    return redirect(to='home')
+    lista = ["Burger","McDonald","PDG","JM"]
+    hijo = Persona("Fernando Rivera","4")
+    contexto={"nombre":"Jose Riquelme","comidas":lista,"hijo":hijo}
+    return render(request,'core/home.html',contexto)
