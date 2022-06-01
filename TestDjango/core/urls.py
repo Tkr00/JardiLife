4db0bd5dff11d44d7b3076a216d309237d73896f
fepriet <fe.prieto@duocuc.cl>
2022-06-01 14:13:34 -0400

    views arregladas

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views  import Pagina, inicio, Flores, Macetero, Arbustos, Producto, quienes_somos, tierra_de_hojas
+from .views  import Pagina,inicio,Flores, Macetero, Arbustos, Producto, quienes_somos, tierra_de_hojas
 
 urlpatterns = [
-    path('pagina', Pagina, name= "Pagina" ),
+    path('', Pagina, name= "Pagina" ),
     path('inicio', inicio, name="inicio"),
-    path('flores', Flores, name="Flores"),
-    path('macetero', Macetero, name="Macetero"),
-    path('arbustos', Arbustos, name="Arbustos"),
-    path('producto', Producto, name="Producto"),
+    path('Flores', Flores, name="Flores"),
+    path('Macetero', Macetero, name="Macetero"),
+    path('Arbustos', Arbustos, name="Arbustos"),
+    path('Producto', Producto, name="Producto"),
     path('quienes-somos', quienes_somos, name= "quienes_somos"),
     path('tierra-de-hojas', tierra_de_hojas,name= "tierra_de_hojas")
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-class Persona:
-    def __init__(self,nombre,edad):
-        self.nombre=nombre
-        self.edad=edad
-        super().__init__()
 
 
-def home(request):
+
+def Pagina(request):
     
-    lista = ["Burger","McDonald","PDG","JM"]
-    hijo = Persona("Fernando Rivera","4")
-    contexto={"nombre":"Jose Riquelme","comidas":lista,"hijo":hijo}
-    return render(request,'core/home.html',contexto)
+    return render(request,'core/Pagina.html',)
+def inicio(request):
+    
+    return render(request,'core/inicio.html',)
+def Flores(request):
+    
+    return render(request,'core/Flores.html',)
+def Macetero(request):
+    
+    return render(request,'core/Macetero.html',)
+def Arbustos(request):
+    
+    return render(request,'core/Arbustos.html',)
+def Producto(request):
+    
+    return render(request,'core/Producto.html',)    
+def quienes_somos(request):
+    
+    return render(request,'core/quienes_somos.html',)   
+def tierra_de_hojas(request):
+    
+    return render(request,'core/tierra_de_hojas.html',)     
+
