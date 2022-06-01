3949affecbd1e8594176e13e025e69d022af81d8
fepriet <fe.prieto@duocuc.cl>
2022-06-01 14:35:49 -0400

    links funcionando

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('Macetero', Macetero, name="Macetero"),
     path('Arbustos', Arbustos, name="Arbustos"),
     path('Producto', Producto, name="Producto"),
-    path('quienes-somos', quienes_somos, name= "quienes_somos"),
-    path('tierra-de-hojas', tierra_de_hojas,name= "tierra_de_hojas")
+    path('quienes_somos', quienes_somos, name= "quienes_somos"),
+    path('tierra_de_hojas', tierra_de_hojas,name= "tierra_de_hojas")
 ]
