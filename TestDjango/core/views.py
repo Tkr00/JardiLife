53e2bd946f358de74992bc4ca9d8e80fddda85f1
ByronRomanA <by.roman@duocuc.cl>
2022-06-03 12:09:07 -0400

    a

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -11,13 +11,25 @@ def inicio(request):
     
     return render(request,'core/inicio.html',)
 def Flores(request):
-    
+    flores = Flores.objects.all()
+
+    datos = {
+        'flores': flores
+    }
     return render(request,'core/Flores.html',)
 def Macetero(request):
-    
+    Macetero = Macetero.objects.all()
+
+    datos = {
+        'macetero': macetero
+    }
     return render(request,'core/Macetero.html',)
 def Arbustos(request):
-    
+    arbustos = Arbustos.objects.all()
+
+    datos = {
+        'arbustos': arbustos
+    }
     return render(request,'core/Arbustos.html',)
 def Producto(request):
     
@@ -26,6 +38,10 @@ def quienes_somos(request):
     
     return render(request,'core/quienes_somos.html',)   
 def tierra_de_hojas(request):
-    
+    tierra_de_hojas = Tierra_de_hojas.objects.all()
+
+    datos = {
+        'tierra_de_hojas': tierra_de_hojas
+    }
     return render(request,'core/tierra_de_hojas.html',)     
 
