2b12c6253c237946a5adfc6b85375dcf46f71bdc
fepriet <fe.prieto@duocuc.cl>
2022-06-09 02:55:52 -0400

    2

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views  import Pagina,inicio,Flores, Macetero, Arbustos, quienes_somos, tierra_de_hojas
+from .views  import Pagina,inicio,Flores, Macetero, Arbustos, quienes_somos, tierra_de_hojas,p
 
 urlpatterns = [
     path('', Pagina, name= "Pagina" ),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('Macetero', Macetero, name="Macetero"),
     path('Arbustos', Arbustos, name="Arbustos"),
     path('quienes_somos', quienes_somos, name= "quienes_somos"),
-    path('tierra_de_hojas', tierra_de_hojas,name= "tierra_de_hojas")
+    path('tierra_de_hojas', tierra_de_hojas,name= "tierra_de_hojas"),
+    path('p', p,name="p")
 ]
