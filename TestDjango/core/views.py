f94ff695165ec9c97fdd88a9746e68c5a350ba6c
fepriet <fe.prieto@duocuc.cl>
2022-06-10 00:03:33 -0400

    falta agrgar mas diseño

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -38,8 +38,9 @@ def tierra_de_hojas(request,id):
     categorias = Categoria.objects.get(idCategoria=id)
     producto=Producto.objects.filter(categoria=categorias)
     return render(request,'core/tierra_de_hojas.html',{'categoria':categorias,'producto':producto})  
+
 def form_produc(request,id):
     nombre = Producto.objects.get(nombreP=id)
     producto=Producto.objects.filter(nombreP=nombre)
-    return render(request,'core/form_produc',{'nombre': nombre,'producto':producto})  
+    return render(request,'core/form_produc.html',{'producto':producto})  
 
