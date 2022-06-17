3276db4390064e661e7ab9c1d946c0ed32902d30
fepriet <fe.prieto@duocuc.cl>
2022-06-16 23:23:19 -0400

    carrito completo falta complementar con el login

--- TestDjango/core/context_processor.py
+++ TestDjango/core/context_processor.py
@@ -1,6 +1,6 @@
 def total_carro(request):
-    total=1000
-    if request.user.is_authenticated:
-        for key, value in request.session["carro"].items():
-            total=total+(float(value["precio"])*value["cantidad"])
+    total=0
+    #if request.user.is_authenticated:
+    for key, value in request.session["carro"].items():
+        total=total+float(value["precio"])
     return {"total_carro":total}
