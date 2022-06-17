f0938f5711c088442f5b2bf7d3c65b6f373c6b18
fepriet <fe.prieto@duocuc.cl>
2022-06-16 21:38:43 -0400

    carrito casi completo

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'core.context_processor.total_carro'
             ],
         },
     },
