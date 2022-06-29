77a1c5b08e39dd713abed2cf4960908538f3c5ed
Tkr00 <al.fariaso@duocuc.cl>
2022-06-29 03:13:13 -0400

    get, put, delete

--- TestDjango/rest_jardi/urls.py
+++ TestDjango/rest_jardi/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from rest_jardi.views import lista_producto
+from rest_jardi.views import lista_producto,detalle_producto
 
 urlpatterns = [
     path('lista_producto', lista_producto, name="lista_producto"),
-    
+    path('detalle_producto/<id>', detalle_producto, name="detalle_producto")
 ]
--- TestDjango/rest_jardi/views.py
+++ TestDjango/rest_jardi/views.py
@@ -23,8 +23,23 @@ def lista_producto(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
-
-
-
-
-
+@api_view(['GET','PUT', 'DELETE'])
+def detalle_producto(request,id):
+    try:
+        producto = Producto.objects.get(nombreP=id)
+    except Producto.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = ProductoSerializer(producto)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProductoSerializer(producto, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        producto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)           
